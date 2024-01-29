2a05471ca29a40076072512e5716abc3ce038e51
Yingying Lu <20095278@mail.wit.ie>
2024-01-29 23:44:49 +0000

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -63,3 +63,15 @@ function findFriends (friends : Friend[], validator : (friend: Friend) => boolea
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Ji')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+
+function addInterest(friend : Friend , interestName : string) : string[] {
+  if(friend.interests) {
+      friend.interests.push(interestName)
+  } else {
+      friend.interests = [interestName]
+  }
+  return friend.interests;
+}
+
+console.log(addInterest(friends[0], 'Politics'))
