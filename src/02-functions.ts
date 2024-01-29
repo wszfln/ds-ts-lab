1e667e2933b523c8ef2b23bdd50a5a77c202c522
Yingying Lu <20095278@mail.wit.ie>
2024-01-29 23:36:34 +0000

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,4 +1,4 @@
-import {Friend, Colleague } from './myTypes';
+import {Friend, Colleague, EmailContact } from './myTypes';
 import {friends, colleagues} from "./01-basics";
 
 function older(f: Friend) : string {
@@ -36,3 +36,23 @@ function addColleague(colleagues : Colleague[], name : string, department: strin
 
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+
+function sortColleagues(
+  colleagues: Colleague[],
+  sorter: (c1: Colleague, c2: Colleague) => number
+): EmailContact[] {
+  const sorted = colleagues.sort(sorter); // Colleague[] inferred
+  const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return result 
+}
+
+console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+function findFriends (friends : Friend[], validator : (friend: Friend) => boolean) : string[] {
+  return friends.filter(validator).map(friend => (friend.name))
+}
+
+console.log(findFriends(friends, (friend) => friend.name.startsWith('Ji')));
+console.log(findFriends(friends, (friend) => friend.age < 35));
